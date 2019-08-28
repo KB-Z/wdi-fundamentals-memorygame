a862de4820598dc1eb24f2c8fe88021acfaca35a
Zachary Donner <zdonner179@gmail.com>
2019-08-28 01:23:53 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,19 @@
 console.log("Up and running!");
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-if (cardsInPlay.length === 2) {
-	alert("You found a match!");
-} else if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("Sorry, try again");
-}
+function checkForMatch () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+	} else {
+  console.log("Sorry, try again.");
+	};
+};
+function flipCard (cardId) {
+	checkForMatch;
+	var cardOne = cards[0];
+	cardsInPlay.push(cardOne);
+	var cardTwo = cards[2];
+	cardsInPlay.push(cardTwo);
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+};
